8c885a3013c85aa137348c8d2796e4f576f2d7f3
ginach15 <ginach1215@gmail.com>
2022-09-11 15:12:01 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
-int main (void){
-puts("\"programing is like bulding multilingual puzzle");
-}
+
+/**
+
+* main - Printing
+
+*
+
+* Return: Always 0 (Successful)
+
+*/
+
+int main(void)
+
+{
+
+puts("\"Programming is like building a multilingual 
+
+puzzle");
+
+return (0);
+
