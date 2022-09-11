cc2e614184f883291ff3eaeeb7fb4b85085b286f
ginach15 <ginach1215@gmail.com>
2022-09-11 15:40:28 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@ int main(void)
  * return 0: exited properly, non_zero otherwise
  */
 
-	puts("\"Programming is like building a multilingual puzzle);
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
