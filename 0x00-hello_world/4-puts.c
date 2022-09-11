fec0795d955bf9f4d09a62c654a8577744a29f99
ginach15 <ginach1215@gmail.com>
2022-09-11 15:27:36 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main(void)
 {
-	puts("\"programing is like bulding multilingual puzzle");
+/**
+ *
+ *  * main - prints the phrase "Programming is like building a multilingual puzzle
+ *
+ *   * Return: 0 if exited properly, non-zero otherwise
+ *
+ *    */
+	puts("\"Programming is like building a multilingual puzzle);
 
 	return (0);
 }
