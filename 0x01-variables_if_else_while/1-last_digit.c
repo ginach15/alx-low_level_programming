20583de65bb69b92225f3ad287deee9b2eb01b5f
ginach15 <ginach1215@gmail.com>
2022-09-12 09:18:15 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -24,6 +24,7 @@ printf("Last digit of %d is %d and is greater than 5\n", n, l);
 else if (l == 0)
 {
 printf("Last digit of %d is %d and is 0\n", n, l);
+
 }
 else
 
@@ -33,5 +34,6 @@ printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
 
 }
 
-return (0); 
+return (0);
+
 }
