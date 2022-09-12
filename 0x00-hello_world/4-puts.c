f1bbfcf7e2fd001c19e91563e24028ecc2bcf39d
ginach15 <ginach1215@gmail.com>
2022-09-11 23:24:38 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,19 +5,13 @@
  * return:always 0
   */
 int main(void)
+	
 {
+
 puts("\"Programming is like building a multilingual puzzle");
 
-/**                                                                                                                 
- *
- *  * main-printing                                                                                                    
- *
- *   *                                                                                                                  
- *
- *    * return:always 0                                                                                                  
- *
- *    */
 
 return (0);
+
 }
 
