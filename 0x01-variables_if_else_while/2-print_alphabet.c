16d2ed422dd1eb99e7bbff94087834aed1d403ad
ginach15 <ginach1215@gmail.com>
2022-09-12 09:46:32 -0700

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,20 +4,15 @@
  * main - lowercase alphabets
  * Return: Always (Success)
  */
-
 int main(void)
 
 {
 
-	char c;
-
-	for (c = 'a'; c <= 'z'; c++)
-
-		putchar(c);
-
-	putchar('\n')
-
-		  return (0);
+char c;
+for (c = 'a'; c <= 'z'; c++)
+putchar(c);
+putchar('\n')
+  return (0);
 }
 
 
