596db826822b5ab42a2a9b461b285619b73ea28c
ginach15 <ginach1215@gmail.com>
2022-09-13 16:46:52 -0700

    888

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,8 +19,8 @@ void jack_bauer(void)
 	{
 		while (minutes <= 59)
 		{
-	          mins_remainder = minutes % 10;
-	          hours_remainder = hours % 10;
+	         mins_remainder = minutes % 10;
+	      	   hours_remainder = hours % 10;
 		_putchar(hours / 10 + '0');
 		_putchar(hours_remainder + '0');
 		_putchar(':');
