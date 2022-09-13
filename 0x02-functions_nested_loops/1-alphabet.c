407ee963ff1157ce6d44022d4244d81029452471
ginach15 <ginach1215@gmail.com>
2022-09-13 14:15:17 -0700

    task0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_alphabet – function to print abc
+ * Return: 0
+ */
+
+
+
+Void print_alphabet(void)
+
+
+
+{
+
+
+
+	Char c;
+
+
+
+	For (c = 'a'; c <= 'z'; c++)
+
+
+
+	{
+
+
+
+		_putchar©;
+
+
+
+	}
+
+
+
+	_putchar('\n');
+
+
+
+}
