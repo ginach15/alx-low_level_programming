c3db2e5f01aaeb99ea5ebd3c67f4309060efce71
ginach15 <ginach1215@gmail.com>
2022-09-13 14:05:31 -0700

    task0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * main - print putchar
- * Return: always 0
-  */
+* main - print putchar
+* Return: always 0
+*/
 
 
 
@@ -11,23 +11,23 @@ int main(void)
 
 {
 
-	_putchar('_');
+_putchar('_');
 
-	_putchar('p');
+_putchar('p');
 
-	_putchar('u');
+_putchar('u');
 
-	_putchar('t');
+_putchar('t');
 
-	_putchar('c');
+_putchar('c');
 
-	_putchar('h');
+_putchar('h');
 
-	_putchar('a');
+_putchar('a');
 
-        _putchar('r');
+_putchar('r');
 
-	_putchar('\n');
+_putchar('\n');
 
-	return (0);
+return (0);
 }
