b1ea95ee84c737af722b211dd92e4b49e2d987f2
ginach15 <ginach1215@gmail.com>
2022-09-13 17:14:31 -0700

    8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,8 +5,7 @@
  * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
  * and resets min
  */
- */
- void jack_bauer(void
+ void jack_bauer(void)
 {
 	int hours = 0;
 	int minutes = 0;
