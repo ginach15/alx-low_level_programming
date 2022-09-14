4aa7bb0f73644dac55e74b062a1d89e146a6a18f
ginach15 <ginach1215@gmail.com>
2022-09-13 17:51:57 -0700

    10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,8 +2,8 @@
 
 /**
  * add - Adds two integers.
- * @num1: The first integer to be added.
- @num2: The second integer to be added
+ * num1: The first integer to be added.
+ * num2: The second integer to be added
  * return: The result of the addition.
  */
 int add(int num1, int num2)
