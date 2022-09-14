00b93e564e16be891662921e2b0841bc46dc6ad6
ginach15 <ginach1215@gmail.com>
2022-09-14 11:09:55 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ k = j * i;
 if (j == 0)
 {
 _putchar(k + '0');
-} 
+ } 
 else if (k < 10 && j != 0)
 {
 _putchar(',');
@@ -39,7 +39,7 @@ _putchar(',');
 _putchar(' ');
 _putchar((k / 100) + '0');
 _putchar(((k / 10) % 10) + '0');
-_putchar((k % 10) + '0');	
+ _putchar((k % 10) + '0');	
 }
 }
 _putchar('\n');
