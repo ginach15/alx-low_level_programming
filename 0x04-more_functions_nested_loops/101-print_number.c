ec2c9ea71583416f5c5d595420cb04185049af17
ginach15 <ginach1215@gmail.com>
2022-09-15 18:47:23 -0700

    12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -5,9 +5,12 @@
 
 
 /**
- * print_number - Prints a number
- * @n: The number to print
- */
+ *
+ * * print_number - Prints a number
+ *
+ * * @n: The number to print
+ *
+ * */
 
 
 
@@ -15,30 +18,30 @@ void print_number(int n)
 
 {
 
-unsigned int num = n;
+	unsigned int num = n;
 
 
 
-if (n < 0)
+	if (n < 0)
 
-{
+	{
 
-putchar('-');
+		putchar('-');
 
-num = -num;
+		num = -num;
+
+	}
+
+	if (num > 9)
+
+	{
+
+		print_number(num / 10);
+
+	}
+
+	putchar(num % 10 + '0');
 
 }
 
-if (num > 9)
-
-{
-
-print_number(num / 10);
-
-}
-
-putchar(num % 10 + '0');
-
-}
-
-
+:
