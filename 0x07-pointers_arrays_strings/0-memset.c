bc12178166b96a10e81f133398bda3c2b47fb191
ginach15 <ginach1215@gmail.com>
2022-09-26 09:53:32 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 /**
- * File: main.h
- * Auth: alx tech (alx yg)
- *description: Header file containing prototypes for all functions
- *used in the 0x06-pointers_arrays_strings directory.
- */
+* File: main.h
+ * Auth: ginach tech (alex yg)
+*description: Header file containing prototypes for all functions
+*used in the 0x06-pointers_arrays_strings directory.
+*/
 #include <stddef.h>
 int _putchar(char c);
 void *_memset(void *s, int c, size_t n);
