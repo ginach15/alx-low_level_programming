80ded9cba277da45755a24595a0b7fb312375b4c
ginach15 <ginach1215@gmail.com>
2022-09-29 11:42:55 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,11 +1,11 @@
 /*
- * File: 0-puts_recursion.c
- * Auth: sam
- */
+* File: 0-puts_recursion.c
+* Auth: sam
+*/
 #include "main.h"
- /**
-  * _puts_recursion - Prints a string, followed by a new line.
-  * @s: The string to be printed.
+/**
+* _puts_recursion - Prints a string, followed by a new line.
+* @s: The string to be printed.
 */
 void _puts_recursion(char *s)
 {
@@ -16,4 +16,4 @@ _puts_recursion(s + 1);
 }
 else
 _putchar('\n');
-}
+
